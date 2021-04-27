d99f5461bc5145a9a56b369b83a83107a2506c17
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-04-27 17:14:18 -0300

    mais configuracoes para pagina inicial

---
+++ resources/js/components/contacts/Index.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		Página vindo do Vue
+	</div>
+</template>
+
+<script>	
+	export default {
+		props: ['contacts'],
+		data() { 
+
+			return {
+				fields: [
+					{ key: 'id', sortable: true, label: 'ID' },
+					{ key: 'name', sortable: true, label: 'Nome' },
+					{ key: 'actions' }
+				]
+			}
+		},
+		methods: {
+		}
+	}
+</script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+mix
+.setPublicPath('public')
+.js('resources/js/app.js', 'public/js').vue()
+    .sass('resources/sass/app.scss', 'public/css');
+    //.sourceMaps();
