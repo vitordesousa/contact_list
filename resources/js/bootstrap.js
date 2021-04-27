fd6032ab48c8a25e7d82e43db06ed923f81f91c1
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-04-27 16:37:46 -0300

    colocando o csrf token para pegar direto do head

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,3 +39,20 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+ let token = document.head.querySelector('meta[name="csrf-token"]');
+
+ if (token) {
+     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+ } else {
+     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+ }
+
+ 
