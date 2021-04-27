8e785b8732d8da7749ed8128b0c8a206b947464b
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-04-27 17:01:33 -0300

    configuracao das packages e adicionada o portal vue que o bootstrap-vue precisa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
 require('./bootstrap');
+
+// vue
+window.Vue = require('vue');
+
+// bootstrap vue
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+
+// used with bootstrap-vue
+import PortalVue from 'portal-vue'
+Vue.use(PortalVue)
+
+// to use mask in fields
+import mask from 'vue-the-mask'
+Vue.use(mask)
+// validate fields form with vue
+import Vuelidate from 'vuelidate'
+Vue.use(Vuelidate)
+
+
+Vue.component('contacts-index', require('./components/contacts/Index.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+	el: '#app',
+});
